67a5c115436e4a66470a439209259ba4f51d83f4
ratiomark <yanagae@gmail.com>
2023-05-03 18:30:27 +0000

    add fronted

--- backend/app.js
+++ backend/app.js
@@ -53,7 +53,7 @@ app.use(errorMiddleware);
 
 function start() {
 	try {
-		const PORT = process.env.PORT || 5000;
+		const PORT = process.env.PORT || 3000;
 		app.listen(PORT, () => {
 			console.log('Сервер запущен на порту ', PORT);
 		});
