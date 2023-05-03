9f103303c7bde5c293f8e60f49ae032020102c8b
ratiomark <yanagae@gmail.com>
2023-05-03 22:49:03 +0300

    add new api url

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -10,6 +10,6 @@ export const validationConfig = {
 	inputElemErrorClass: 'input_error',
 }
 export const myToken = 'df023b85-a5b5-4c38-8fae-b6cacb3ba5fd'
-export const baseUrl = 'api.ratiomark.nomoredomains.monster'
-export const AuthAPIbaseUrl = 'api.ratiomark.nomoredomains.monster'
+export const baseUrl = 'https://api.ratiomark.nomoredomains.monster'
+export const AuthAPIbaseUrl = 'https://api.ratiomark.nomoredomains.monster'
 export const LS_TOKEN_KEY = 'jwt'
