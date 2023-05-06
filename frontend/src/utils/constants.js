b610d0b456c17333e3f3bee8496eecd0dd3c277b
ratiomark <yanagae@gmail.com>
2023-05-06 21:01:50 +0300

    fix token, add Api context

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -10,8 +10,6 @@ export const validationConfig = {
 	inputElemErrorClass: 'input_error',
 }
 export const myToken = 'df023b85-a5b5-4c38-8fae-b6cacb3ba5fd'
-export const baseUrl = 'http://localhost:4000'
-export const AuthAPIbaseUrl = 'http://localhost:4000'
-// export const baseUrl = 'https://api.ratiomark.nomoredomains.monster'
-// export const AuthAPIbaseUrl = 'https://api.ratiomark.nomoredomains.monster'
+export const baseUrl = 'https://api.ratiomark.nomoredomains.monster'
+export const AuthAPIbaseUrl = 'https://api.ratiomark.nomoredomains.monster'
 export const LS_TOKEN_KEY = 'jwt'
